b0550b816038d7dbfd9397c4839950ae59b6e379
mallikarjun meti <89732992+mgmeti@users.noreply.github.com>
2024-12-05 11:26:01 +0530

    Added 5 buttons problem
    It related to maintaining block scoped state

--- interview-questions.js
+++ interview-questions.js
@@ -27,6 +27,59 @@ console.log(add(2)(3));
 })();
 
 
+// Five button problem
+// 1. using IIFE
+function createButtons() {
+   for (var i = 1; i <= 5; i++) {
+     var body = document.getElementsByTagName("BODY")[0];
+     var button = document.createElement("BUTTON");
+     button.innerHTML = 'Button ' + i;
+  
+     (function (num){
+          button.onclick = function() {
+          alert('This is button ' + num);
+     }
+     })(i);
+     body.appendChild(button);
+   }
+} 
+createButtons();
+
+// 2. Using let keyword as it is block scoped
+function createButtons() {
+   for (let i = 1; i <= 5; i++) {
+     var body = document.getElementsByTagName("BODY")[0];
+     var button = document.createElement("BUTTON");
+     button.innerHTML = 'Button ' + i;
+     button.onclick = function(){
+       alert('Button '+ i );
+     }
+
+     body.appendChild(button);
+   }
+} 
+createButtons();
+
+// 3. Using a function 
+function createButtons() {
+   for (var i = 1; i <= 5; i++) {
+     var body = document.getElementsByTagName("BODY")[0];
+     var button = document.createElement("BUTTON");
+     button.innerHTML = 'Button ' + i;
+     addClickFunctionality(button, i);
+
+
+     body.appendChild(button);
+   }
+} 
+createButtons();
+function addClickFunctionality(button, num){
+       button.onclick = function(){
+       alert('Button '+ num );
+     }  
+}
+
+
 
 
 
