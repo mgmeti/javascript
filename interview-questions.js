131664bf74d815878ae8097a60f74ca46e184a47
mallikarjun meti <89732992+mgmeti@users.noreply.github.com>
2024-12-05 10:39:54 +0530

    triple add function example

--- interview-questions.js
+++ interview-questions.js
@@ -1,1 +1,23 @@
 
+function tripleAdd(x){
+  return function(y){
+    return function(z){
+      return x + y + z;
+    }
+  }
+}
+
+console.log(tripleAdd(10)(20)(30))
+
+function add(x){
+  return function(y){
+    return x + y;
+  }
+}
+
+console.log(add(2)(3));
+
+
+
+
+
